43b6b54740b7d90a0c144fab257ab8372d017120
decodeme1412 <111673169+decodeme1412@users.noreply.github.com>
2023-06-23 11:04:29 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+This is a basic Google Chrome Extension that shows currect matches in real time.
+![Alt text](https://github.com/arpanbasu1412/chrome-extension/blob/main/screenshot1.png)
+![Alt text](https://github.com/arpanbasu1412/chrome-extension/blob/main/screenshot2.png)
---
+++ script.js
@@ -0,0 +1,24 @@
+async function getMatchData() {
+
+    return await fetch("https://api.cricapi.com/v1/currentMatches?apikey=cf918854-8cf5-42a2-ab55-c5748c2994ad&offset=0")
+        .then(data => data.json())
+        .then(data => {
+            if (data.status != "success")return;
+
+            const matchesList = data.data;
+
+            if(!matchesList)return [];
+            
+            const relevantData = matchesList.map(match => `${match.name}, ${match.status}`);
+
+            console.log({relevantData});
+
+            document.getElementById("matches").innerHTML = relevantData.map(match => `<li>${match} </li>`).join('');
+
+            return relevantData;
+
+        })
+        .catch(e => console.log(e));
+}
+
+getMatchData();
